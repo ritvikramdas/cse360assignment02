90948e1d58fcc8fba9480f926124527970b5119f
Ritvik <Ritvik@LAPTOP-7MCP9A0O>
2020-10-04 18:55:45 -0700

    Initial checkin of AddingMachine tool.

---
+++ cseassignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,43 @@
+package cse360assignment02;
+
+
+// AddingMachine public class
+public class AddingMachine {
+	
+  // private int variable "total"
+  private int total;
+  
+  // Constructor for AddingMachine
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  // public method getTotal
+  // @return	int
+  public int getTotal () {
+    return 0;
+  }
+  
+  // public method add
+  // @param		value (int)
+  // @return	void
+  public void add (int value) {
+  }
+
+  // public method subtract
+  // @param		value (int)
+  // @return	void
+  public void subtract (int value) {
+  }
+
+  // public method toString
+  // @return	String
+  public String toString () {
+    return "";
+  }
+
+  // public method clear
+  // @return	void
+  public void clear() {
+  }
+}
