c5f0484ac2700e89c26bc8d706bd92a158be7206
Ritvik <Ritvik@LAPTOP-7MCP9A0O>
2020-10-04 19:41:45 -0700

    Update all operations for required methods.

--- cseassignment02/src/cse360assignment02/AddingMachine.java
+++ cseassignment02/src/cse360assignment02/AddingMachine.java
@@ -5,39 +5,55 @@ package cse360assignment02;
 public class AddingMachine {
 	
   // private int variable "total"
+  // total stores the result of operations
   private int total;
   
+  // private String variable "MyHistory"
+  // myHistory stores the history of all operations
+  private String myHistory = "0";
+  
   // Constructor for AddingMachine
+  // sets total variable to 0
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   // public method getTotal
+  // returns total
   // @return	int
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   // public method add
   // @param		value (int)
+  // adds total to value and updates myHistory
   // @return	void
   public void add (int value) {
+	  total += value;
+	  myHistory += " + " + value; 
   }
 
   // public method subtract
   // @param		value (int)
+  // subtracts total from value and updates myHistory
   // @return	void
   public void subtract (int value) {
+	  total -= value;
+	  myHistory += " - " + value; 
   }
 
   // public method toString
+  // returns myHistory
   // @return	String
   public String toString () {
-    return "";
+    return myHistory;
   }
 
   // public method clear
+  // clears myHistory
   // @return	void
   public void clear() {
+	  myHistory = "0";
   }
 }
